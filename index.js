7259d757cadc29947ec948f38bf658b0334bf7a8
Sandra G <sandyjtech@gmail.com>
2023-06-21 13:43:28 -0700

    added click event to change dish

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 // Write your code here...
 const menuItemContainer = document.querySelector('#menu-items');
+const dishContainer = document.querySelector('#dish');
+let dishImage = document.querySelector('#dish-image');
+let dishName = document.querySelector('#dish-name');
+let dishDescription = document.querySelector('#dish-description');
+let dishPrice = document.querySelector('#dish-price');
 fetch('http://localhost:3000/menu')
 .then(response => response.json())
 .then((data) => renderFoodList(data));
 
 function renderFoodList(menuItems){
 menuItems.forEach((food) => {
-    const span = document.createElement('span');
-    span.textContent = food.name;
-    const img = document.createElement('img');
-    img.src = food.image;
-    const h6 = document.createElement('h6');
-    h6.textContent = food.description;
-    const p  = document.createElement('p');
-    p.textContent = food.price;
-   menuItemContainer.appendChild(span);
-   menuItemContainer.appendChild(img);
-   menuItemContainer.appendChild(h6);
-   menuItemContainer.appendChild(p);
+const span = document.createElement('span');
+span.textContent = food.name;      
+menuItemContainer.appendChild(span);
+span.setAttribute('id', food.id);
+////////////Dish Section
+dishName.textContent = food.name; 
+dishImage.src = food.image; 
+dishDescription.textContent = food.description;   
+dishPrice.textContent = food.price;
+dishContainer.appendChild(dishImage);
+dishContainer.appendChild(dishName);
+dishContainer.appendChild(dishDescription);
+dishContainer.appendChild(dishPrice);
+
+
+//Event listener to change dish item
+span.addEventListener('click', (e) => {
+e.preventDefault();
+console.log(e.target.id.value);
+dishName.textContent = food.name; 
+dishImage.src = food.image; 
+dishDescription.textContent = food.description;   
+dishPrice.textContent = food.price;
+});
 })
+ 
 }
 
 // "id": 1,
