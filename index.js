cf7de1132d2e2aa55de4540c5fca38647c2e4120
Sandra G <sandyjtech@gmail.com>
2023-06-21 14:17:24 -0700

    added click to update the cart

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ let dishImage = document.querySelector('#dish-image');
 let dishName = document.querySelector('#dish-name');
 let dishDescription = document.querySelector('#dish-description');
 let dishPrice = document.querySelector('#dish-price');
+let numberInCart = document.getElementById("number-in-cart");
+let cartForm = document.getElementById("cart-form");
+
 fetch('http://localhost:3000/menu')
 .then(response => response.json())
 .then((data) => renderFoodList(data));
 
 function renderFoodList(menuItems){
+
 menuItems.forEach((food) => {
 const span = document.createElement('span');
 span.textContent = food.name;      
@@ -20,23 +24,32 @@ dishName.textContent = food.name;
 dishImage.src = food.image; 
 dishDescription.textContent = food.description;   
 dishPrice.textContent = food.price;
+numberInCart.textContent = food.number_in_bag;
 dishContainer.appendChild(dishImage);
 dishContainer.appendChild(dishName);
 dishContainer.appendChild(dishDescription);
 dishContainer.appendChild(dishPrice);
 
-
 //Event listener to change dish item
 span.addEventListener('click', (e) => {
 e.preventDefault();
-console.log(e.target.id.value);
+
 dishName.textContent = food.name; 
 dishImage.src = food.image; 
 dishDescription.textContent = food.description;   
-dishPrice.textContent = food.price;
+numberInCart.textContent = food.number_in_bag;
 });
+
+
 })
- 
+//Update cart number on click
+cartForm.addEventListener('submit', (e) => {
+    let input  = document.querySelector("#cart-amount");
+    e.preventDefault()
+    console.log(e);
+     numberInCart.textContent = parseInt(input.value) + parseInt(numberInCart.textContent);
+   
+})
 }
 
 // "id": 1,
